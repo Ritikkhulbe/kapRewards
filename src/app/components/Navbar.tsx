0aa62ca46d9d6f591bd9fb17ce4e483a1d88eb7f
Ritik <ritikkhulbe22@gmail.com>
2025-01-19 21:54:37 +0530

    fix@4

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -7,40 +7,39 @@ import { Separator } from "@/components/ui/separator";
 import { withPublicUrl } from "@/util";
 
 const Navbar = () => {
-    // Helper function to remove any trailing `/*` from the path
-    const cleanPath = (path: string) => path.replace(/\/\*$/, "");
+  // Helper function to remove any trailing `/*` from the path
+  const cleanPath = (path: string) => path.replace(/\/\*$/, "");
 
-    return (
-        <div className="w-14 bg-white h-screen border-r border-[#ECEEF1] fixed overflow-hidden">
-            <nav className="flex flex-col h-[100%] justify-between">
+  return (
+    <div className="w-14 bg-white h-screen border-r border-[#ECEEF1] fixed overflow-hidden">
+      <nav className="flex flex-col h-[100%] justify-between">
+        {/* Side Bar Header */}
+        <ul className="flex flex-col justify-center items-center w-full ">
+          <div className="my-4 flex justify-center items-center">
+            <Logo />
+          </div>
+          {routesConfig.map((route) => (
+            <li key={route.path} className="my-1">
+              <NavLink
+                to={withPublicUrl(cleanPath(route.path))}
+                className="flex justify-center items-center"
+              >
+                {({ isActive }) => <route.icon isActive={isActive} />}
+              </NavLink>
+            </li>
+          ))}
+        </ul>
 
-                {/* Side Bar Header */}
-                <ul className="flex flex-col justify-center items-center w-full ">
-                    <div className="my-4 flex justify-center items-center">
-                        <Logo />
-                    </div>
-                    {routesConfig.map((route) => (
-                        <li key={route.path} className="my-1">
-                            <NavLink to={withPublicUrl(cleanPath(route.path))} className="flex justify-center items-center">
-                                {({ isActive }) => <route.icon isActive={isActive} />}
-                            </NavLink>
-                        </li>
-                    ))}
-                </ul>
-
-                {/* Side Bar Footer */}
-                <div className="mb-4 flex flex-col justify-center items-center">
-                    <Separator className="mb-3 w-[80%]" />
-                    <NavLink to={withPublicUrl("/settings")} className="flex justify-center items-center my-2">
-                        {({ isActive }) => <SettingsIcon isActive={isActive} />}
-                    </NavLink>
-                    <div className="flex justify-center items-center">
-                        <AgentIcon />
-                    </div>
-                </div>
-            </nav>
+        {/* Side Bar Footer */}
+        <div className="mb-4 flex flex-col justify-center items-center">
+          <Separator className="mb-3 w-[80%]" />
+          <div className="flex justify-center items-center">
+            <AgentIcon />
+          </div>
         </div>
-    );
+      </nav>
+    </div>
+  );
 };
 
 export default Navbar;
