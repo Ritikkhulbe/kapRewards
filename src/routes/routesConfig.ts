1b120f4bbb8f72c4a2b876ca796412940e37c859
MOHIT KUMAR SHAW <mohitshawmath@gmail.com>
2025-01-19 15:46:27 +0530

    rendering here

--- src/routes/routesConfig.ts
+++ src/routes/routesConfig.ts
@@ -1,11 +1,15 @@
-import CoachingIcon from "@/app/assets/icons/CoachingIcon";
+// import CoachingIcon from "@/app/assets/icons/CoachingIcon";
 import ConversationsIcon from "@/app/assets/icons/ConversationsIcon";
 import DashboardIcon from "@/app/assets/icons/DashboardIcon";
 import ScoreCardIcon from "@/app/assets/icons/ScoreCardIcon";
-import CoachingRoutes from "@/app/features/coaching/CoachingRoutes";
+// import CoachingRoutes from "@/app/features/coaching/CoachingRoutes";
 import ConversationRoutes from "@/app/features/conversations/ConversationRoutes";
 import DashboardRoutes from "@/app/features/dashboard/DashboardRoutes";
 import scorecardRoutes from "@/app/features/scorecards/scorecardRoutes";
+import RewardsPage from "@/app/components/RewardsPage";
+import GiftNotification from "@/app/components/GiftNotification";
+import ProductPage from  "@/app/components/Products"
+import OrderPage from  "@/app/components/OrderPage"
 
 interface RouteConfig {
   path: string;
@@ -31,6 +35,30 @@ const routesConfig: RouteConfig[] = [
     path: "/scorecard/*",
     element: scorecardRoutes,
     title: "Score Card",
+    icon: ScoreCardIcon,
+  },
+  {
+    path: "/reward/*",
+    element: RewardsPage,
+    title: "Card",
+    icon: ScoreCardIcon,
+  },
+  {
+    path: "/notify/*",
+    element: GiftNotification,
+    title: "Card",
+    icon: ScoreCardIcon,
+  },
+  {
+    path: "/product/*",
+    element: ProductPage,
+    title: "Card",
+    icon: ScoreCardIcon,
+  },
+  {
+    path: "/order/*",
+    element: OrderPage,
+    title: "Card",
     icon: ScoreCardIcon,
   },
 ];
