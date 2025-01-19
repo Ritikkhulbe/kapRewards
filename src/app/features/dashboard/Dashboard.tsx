1f771032b91bf2e9014dd3adb5ff975b2172d753
Ritik <ritikkhulbe22@gmail.com>
2025-01-19 17:03:25 +0530

    fix#6

--- src/app/features/dashboard/Dashboard.tsx
+++ src/app/features/dashboard/Dashboard.tsx
@@ -1,31 +1,43 @@
-import GenericLayout, { GenericLayoutButton, GenericLayoutDescription, GenericLayoutHeader, GenericLayoutNotification } from "@/app/genericComponents/GenericLayout"
-import withReducer from "@/app/utils/withReducer"
-import dashboardSlice from "./store/slice/dashboardSlice"
-import { useEffect } from "react"
-import { useAppDispatch } from "@/store/hooks"
-import { exampleAction } from "./store/actions/exampleAction"
-import DashboardHeader from "./components/DashboardHeader"
+import GenericLayout, {
+  GenericLayoutButton,
+  GenericLayoutDescription,
+  GenericLayoutHeader,
+  GenericLayoutNotification,
+} from "@/app/genericComponents/GenericLayout";
+import withReducer from "@/app/utils/withReducer";
+import dashboardSlice from "./store/slice/dashboardSlice";
+import { useEffect } from "react";
+import { useAppDispatch } from "@/store/hooks";
+import { exampleAction } from "./store/actions/exampleAction";
+import DashboardHeader from "./components/DashboardHeader";
 
 const Dashboard = () => {
+  const dispatch = useAppDispatch();
 
-  const dispatch = useAppDispatch()
-  
-  useEffect(()=>{
-    console.log("Dashboard component mounted")
-    dispatch(exampleAction("Hello World"))
-  },[])
+  useEffect(() => {
+    console.log("Dashboard component mounted");
+    dispatch(exampleAction("Hello World"));
+  }, []);
 
   return (
     <GenericLayout>
       <GenericLayoutHeader> Kap Rewards </GenericLayoutHeader>
       <GenericLayoutNotification>5</GenericLayoutNotification>
-      <GenericLayoutDescription> Your current Rewards Portal</GenericLayoutDescription>
+      <GenericLayoutDescription>
+        {" "}
+        Your current Rewards Portal
+      </GenericLayoutDescription>
       <GenericLayoutButton>
-        <div className="px-5 py-2 rounded-[20px] bg-blue-500">5 Reward Points</div>
+        <div className="flex items-center gap-2">
+          <div className="px-5 py-2 rounded-[20px] bg-blue-500">
+            5 Reward Points
+          </div>
+          <div className="px-5 py-2 rounded-[20px] bg-fourth">Ritik</div>
+        </div>
       </GenericLayoutButton>
       <DashboardHeader />
     </GenericLayout>
-  )
-}
+  );
+};
 
-export default withReducer("dashboard", dashboardSlice)(Dashboard)
+export default withReducer("dashboard", dashboardSlice)(Dashboard);
